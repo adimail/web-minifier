64ccfe4220f3955734b11b2a1c94daa64b4df388
Aditya <adimail2404@gmail.com>
2024-12-08 02:59:17 +0530

    `--watch` flag to rebuild live file changes

--- minify.pl
+++ minify.pl
@@ -5,6 +5,8 @@ use JSON;
 use File::Basename;
 use File::Path  qw(make_path);
 use Time::HiRes qw(gettimeofday tv_interval);
+use File::ChangeNotify;
+use File::Basename;
 
 sub minify_js {
 	my ($code) = @_;
@@ -47,9 +49,7 @@ sub minify_css {
 	return $code;
 }
 
-my $old_size = 0;
-my $new_size = 0;
-my $time     = 0;
+my ( $old_size, $new_size, $time ) = ( 0, 0, 0 );
 
 # Color codes for output
 my $RED   = "\033[31m";
@@ -106,9 +106,7 @@ sub process_file {
 
 	my $original_size = -s $input_file;
 	my $minified_size = -s $output_file;
-	print "Time taken: $time_taken seconds\n";
-	print "Original file size: $original_size bytes\n";
-	print "Minified file size: $minified_size bytes\n";
+	print "$original_size -> $minified_size bytes \t in $time_taken\n";
 	print_size_diff( $original_size, $minified_size );
 
 	$old_size += $original_size;
@@ -116,7 +114,7 @@ sub process_file {
 	$time     += $time_taken;
 }
 
-my $config_file = 'config.json';
+my $config_file = 'web-minifier.config.json';
 open my $config_fh, '<', $config_file or die "Cannot open '$config_file': $!\n";
 my $config_content = do { local $/; <$config_fh> };
 close $config_fh;
@@ -135,63 +133,102 @@ my $output_dir_css = $config->{css}->{output}
 make_path($output_dir_js)  unless -d $output_dir_js;
 make_path($output_dir_css) unless -d $output_dir_css;
 
-my $param = shift;
+# Watch mode
+my $watch_mode = grep { $_ eq '--watch' } @ARGV;
+if ($watch_mode) {
+	print "Starting in watch mode...\n";
 
-if ( defined $param ) {
-	if ( -f $param && $param =~ /\.js$/ ) {
-		process_file( $param, basename($param), \&minify_js );
-	}
-	elsif ( -f $param && $param =~ /\.css$/ ) {
-		process_file( $param, basename($param), \&minify_css );
-	}
-	else {
-		print
-"You can only pass one parameter and it must be either a JS or CSS file.\n";
+	# Instantiate the watcher object
+	my $watcher = File::ChangeNotify->instantiate_watcher(
+		directories => [ $input_dir_js, $input_dir_css ],
+		filter      => qr/\.(js|css)$/,    # Only watch .js and .css files
+	);
+
+	while (1) {
+
+		# Wait for events
+		my @events = $watcher->wait_for_events;
+
+		for my $event (@events) {
+
+			# Check the event type
+			next unless $event->type eq 'modify' || $event->type eq 'create';
+
+			my $file = $event->path;
+			if ( $file =~ /\.js$/ ) {
+				process_file( $file, "$output_dir_js/" . basename($file),
+					\&minify_js );
+			}
+			elsif ( $file =~ /\.css$/ ) {
+				process_file( $file, "$output_dir_css/" . basename($file),
+					\&minify_css );
+			}
+		}
 	}
 }
 else {
-	for my $dir (
-		[
-			$input_dir_js, $output_dir_js, \&minify_js,
-			'.js', $config->{js}->{ignore}
-		],
-		[
-			$input_dir_css, $output_dir_css,
-			\&minify_css,   '.css',
-			$config->{css}->{ignore}
-		]
-	  )
-	{
-		my ( $input_dir, $output_dir, $minify_sub, $ext, $ignore_list ) = @$dir;
-		my %ignore = map { $_ => 1 } @$ignore_list;
+	print "Run the script with --watch to enable watch mode.\n\n\n";
 
-		if ( -d $input_dir ) {
-			opendir my $dh, $input_dir
-			  or die "Cannot open directory '$input_dir': $!\n";
-			while ( my $file = readdir $dh ) {
-				next
-				  unless $file =~ /\Q$ext\E$/;
-				next if $ignore{$file};
-				process_file( "$input_dir/$file", "$output_dir/$file",
-					$minify_sub );
-			}
-			closedir $dh;
+	my $param = shift;
+
+	if ( defined $param ) {
+		if ( -f $param && $param =~ /\.js$/ ) {
+			process_file( $param, basename($param), \&minify_js );
+		}
+		elsif ( -f $param && $param =~ /\.css$/ ) {
+			process_file( $param, basename($param), \&minify_css );
 		}
 		else {
-			print "The input directory for $ext files does not exist.\n";
+			print
+"You can only pass one parameter and it must be either a JS or CSS file.\n";
+		}
+	}
+	else {
+		for my $dir (
+			[
+				$input_dir_js, $output_dir_js,
+				\&minify_js,   '.js',
+				$config->{js}->{ignore}
+			],
+			[
+				$input_dir_css, $output_dir_css,
+				\&minify_css,   '.css',
+				$config->{css}->{ignore}
+			]
+		  )
+		{
+			my ( $input_dir, $output_dir, $minify_sub, $ext, $ignore_list ) =
+			  @$dir;
+			my %ignore = map { $_ => 1 } @$ignore_list;
+
+			if ( -d $input_dir ) {
+				opendir my $dh, $input_dir
+				  or die "Cannot open directory '$input_dir': $!\n";
+				while ( my $file = readdir $dh ) {
+					next
+					  unless $file =~ /\Q$ext\E$/;
+					next if $ignore{$file};
+					process_file( "$input_dir/$file", "$output_dir/$file",
+						$minify_sub );
+				}
+				closedir $dh;
+			}
+			else {
+				print "The input directory for $ext files does not exist.\n";
+			}
 		}
 	}
 }
 
-print "-" x 50, "\n";
-print "Summary:\n";
-print "Total time taken: $time seconds\n";
-print "Total original size: $old_size bytes\n";
-print "Total minified size: $new_size bytes\n";
-my $total_diff = $old_size - $new_size;
-my $percentage_reduction =
-  $old_size > 0
-  ? sprintf( "%.2f", ( $total_diff / $old_size ) * 100 )
-  : 0;
-print "Total size reduced: $total_diff bytes ($percentage_reduction%)\n";
-print "-" x 50, "\n";
+END {
+	print "-" x 50, "\n";
+	print "Summary:\n";
+	print "Total time taken: $time seconds\n";
+	print "Total original size: $old_size bytes\n";
+	print "Total minified size: $new_size bytes\n";
+	my $total_diff = $old_size - $new_size;
+	my $percentage_reduction =
+	  $old_size > 0 ? sprintf( "%.2f", ( $total_diff / $old_size ) * 100 ) : 0;
+	print "Total size reduced: $total_diff bytes ($percentage_reduction%)\n";
+	print "-" x 50, "\n";
+}
